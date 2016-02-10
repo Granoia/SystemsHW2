b92f10fa4433112a554d2632d04cf56262e400bb
Joshua Aaron Hepworth <jhepwort@banku.reed.edu>
2016-02-09 17:07:06 -0800

    plotLooper now iterates inversely prop. to size

--- plotLooper.py
+++ plotLooper.py
@@ -2,12 +2,12 @@ import sys, os
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("plotLooper <file> <output>")
+        print("plotLooper <file> <output> ")
         os.sys.exit(-1)
     filename = sys.argv[1]
     output = sys.argv[2]
     for i in range(8,24):
         size = str(2**i)
-        iter = str(1)
+        iter = str(2**24 / i)
         os.sys('filename ' + size + ' ' + iter + ' ' + output)
     os.sys.exit(0)
