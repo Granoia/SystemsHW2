0579bb9eec36385324fd31f9fab1370156d7d650
Joshua Aaron Hepworth <jhepwort@banku.reed.edu>
2016-02-11 22:40:36 -0800

    Added data and renamed double_c implementation

--- update_locations_double.c
+++ update_locations_double.c
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdint.h>
+#include <sys/sysinfo.h>
+#define BILLION 1000000000
+
+FILE *fileout;
+const char outputFilename[] = "temp.txt";
+
 
 /*generate a list of random doubles within the interval [-bound, bound], length 'size'*/
 double* generate_random_list(uint64_t size, double bound){
@@ -16,26 +22,30 @@ double* generate_random_list(uint64_t size, double bound){
   return ret;
 }
 
+/*update location by velocity, one time-step*/
 void update_coords(uint64_t size, double* x, double* y, double* z, double* vx, double* vy, double* vz)
 {
-  for (uint64_t i=0; i < 8; i++)
-    {
-      asm("addq %3, %0; "
-	  :"=r"(x[i]), "=r"(y[i]), "=r"(z[i])
-	  :"r"(vx[i]), "r" (vy[i]), "r"(vz[i])
-      :"memory");
-    }
+  for(uint64_t i=0; i<size; i++)
+  {
+    x[i] += vx[i];
+    y[i] += vy[i];
+    z[i] += vz[i];
+  }
 }
 
+
 int main(int argc, char** argv){
-  if(argc != 3){
+  if(argc < 3){
     printf("args: <size> <iters>\n");
     return -1;
   }
-  srand(time(NULL));
   int size = 1 << atoi(argv[1]);
   int iters = 1 << atoi(argv[2]);
-  printf("size: %d\n",size);
+
+  struct sysinfo info;
+
+  srand(size);
+
 
   double* x = generate_random_list(size,1000.);
   double* y = generate_random_list(size,1000.);
@@ -45,22 +55,26 @@ int main(int argc, char** argv){
   double* vz = generate_random_list(size,1.);
 
   struct timespec start,end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+  for (uint64_t i=0; i < iters; i++) update_coords(size,x,y,z,vx,vy,vz);
+  
   clock_gettime(CLOCK_MONOTONIC, &end);
 
-  for (uint64_t i=0; i < iters; i++) 
-  {
-    update_coords(size,x,y,z,vx,vy,vz);
-  }  
-  clock_gettime(CLOCK_MONOTONIC, &end);
-
-  double checksum = 0;
-    for (int i=0; i < size; i++) {
-      printf("%f\n",x[i]);
-    }
-
-  double elapsed = end.tv_nsec - start.tv_nsec;
+  double elapsed = (end.tv_sec * BILLION + end.tv_nsec) - (start.tv_sec * BILLION + start.tv_nsec);
   double avg = elapsed/(double)(size * iters);
-  printf("\nAverage Elapsed Time: %f\n",avg);
-  printf("checksum: %f\n", checksum);
+  int checksum = 0;
+  for (int i=0; i<size; i++){
+    checksum += x[i] + y[i] + z[i];
+  }
+
+  sysinfo(&info);
+  printf("uptime: %d",info.uptime);
+
+  fileout = fopen(outputFilename, "a"); 
+  fprintf(fileout, "%f\n",avg);
+  fclose(fileout);
+  printf("checksum is %d\n",checksum);
+  printf("Average Elapsed Time: %fus\n",avg);
   return 0;
 }
