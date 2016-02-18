627ed364eaa06192d5d608bfc7ff24bf3881df5e
Joshua Aaron Hepworth <jhepwort@banku.reed.edu>
2016-02-17 21:59:38 -0800

    First Example

--- hw2.c
+++ hw2.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
+
+uint8_t* make_buffer(int len)
+{
+  uint8_t* ret = malloc(8*len);
+  for (int i=0; i < len; i++)
+    {
+      ret[i] = rand() % (1<<8 - 1);
+    }
+  return ret;
+}
 
 
 
 int main()
 {
+  srand(time(NULL));
 
+  uint_t* buffer = make_buffer(20);
+  for (int i=0; i < 20; i++)
+    {
+      printf("\n");
+      printf("%d, ",buffer[i]);
+    }
 }
