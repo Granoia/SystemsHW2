08b33774e2c1e236877ff169ed3c2ac8afa5e944
Joshua Aaron Hepworth <jhepwort@ravioli.reed.edu>
2016-02-22 12:27:16 -0800

    Copies one array to another. Segfault at 2^29 for some reason.

--- hw2.c
+++ hw2.c
@@ -5,9 +5,9 @@
 
 #define BILLION 1000000000
 
-uint64_t* make_buffer(int len)
+uint8_t* make_buffer(int len)
 {
-  uint64_t* ret = malloc(64*len);
+  uint8_t* ret = malloc(8*len);
   for (int i=0; i < len; i++)
     {
       ret[i] = rand() % len;
@@ -15,39 +15,58 @@ uint64_t* make_buffer(int len)
   return ret;
 }
 
-void access_at_random(uint64_t* buffer, int size)
+
+void access_at_random(uint8_t* buffer, int size,uint8_t* trash)
 {
-  uint64_t datum = buffer[0];
   for (int i=0; i < size; i++)
     {
-      datum = buffer[datum];
+      trash[i] = buffer[i];
     }
-  buffer[0] = (datum * 15485863) % size;
-  printf("%d\n",(int)datum);
 }
 
-
+//Shuffles a list of ints
+void shuffle(uint8_t* ls, int length){
+  printf("Length of list is: %d\n", length);
+  for(int i=0; i<length; i++){
+    int j = rand() % (length-i);
+    uint8_t a = ls[i];
+    uint8_t b = ls[i+j];
+    ls[i] = b;
+    ls[i+j] = a;
+  }
+  return;
+}
 
 int main(int argc, char** argv)
 {
-  if (argc != 3) printf("%s <2^size> <2^iters>\n",argv[0]);
+  printf("%lu\n",sizeof(int));
+  if (argc != 3) 
+  {
+    printf("%s <2^size> <2^iters>\n",argv[0]);
+    return 0;
+  }
 
   const int size = 1 << atoi(argv[1]);
   const int iters = 1 << atoi(argv[2]);
+ 
+  //Initialization
   srand(time(NULL));
   struct timespec begin;
   struct timespec end;
-
-  uint64_t* buffer = make_buffer(size);
+  uint8_t* buffer = make_buffer(size);
+  uint8_t* trash = malloc(8*size);
   printf("Buffer loaded.\n");
 
+  //Timing the loop
   clock_gettime(CLOCK_MONOTONIC, &begin);
   for (int i=0; i < iters; i++) 
   {
-    access_at_random(buffer, size);
+    access_at_random(buffer, size,trash);
   }
   clock_gettime(CLOCK_MONOTONIC, &end);
-
+  free(buffer);
+  free(trash);
+  //Output the time
   double duration = BILLION*(end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec);
   duration /= iters*size;
   printf("Duration: %fns",duration);
