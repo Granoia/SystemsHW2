aea2208eb6e09b0ddaa017c27564f4997eaa71bd
ZEPLAR <Zeplar1@gmail.com>
2016-02-25 22:44:08 -0800

    Added graphs for model2

--- plot.py
+++ plot.py
@@ -19,15 +19,16 @@ def plot(filename):
     y = map(float,getColumn(filename,2))
     length = len(x)
     colors = ['red', 'green', 'blue']
-
     plt.figure(getHeader(filename))
-    plt.title('linear scale')
+    plt.title('log scale')
     plt.xlabel("Array Size")
     plt.ylabel("Speed / Value (ns)")
     plt.ylim((min(y)*1.2,max(y)*1.2))
-    plt.yticks([0,0.5,1,2,3,4,5,6,7,8])
-    plt.xlim((6,25))
+    plt.xlim((6,30))
     plt.xticks(range(8,30,2), [r"$2^{ " + str(i) + "}$" for i in range(8,30,2)])
+    plt.yscale('symlog')
+    ticks = [.1,1,2,4,6,8,10,20,40,80,100]
+    plt.yticks(ticks, map(str,ticks))
     plt.scatter(x, y)
 
 
